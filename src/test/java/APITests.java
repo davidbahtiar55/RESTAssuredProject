cdea92a157492689e4dd0bdd751914b48f957317
david bahtiar <davidbahtiar55@gmail.com>
2022-10-14 16:57:30 +0700

    Update
    (+) Add Folder test-output & file code asset

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,8 +1,10 @@
+package test;
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 public class APITests {
 
     @Test
@@ -22,10 +24,14 @@ public class APITests {
 
     @Test
     void test2(){
-        given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200);
-    }
 
+        baseURI = "https://reqres.in/api";
+
+        given().
+                get("/users?page=2").
+                then().
+                statusCode(200).
+                body("data[1].id", equalTo(8))
+                .log().all();
+    }
 }
